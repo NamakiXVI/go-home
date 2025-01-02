19f0e9dca8b02a36d6473e69ed9e43dfa5da21c5
NamakiXVI <107111904+NamakiXVI@users.noreply.github.com>
2025-01-02 10:57:01 +0100

    commit

--- main.java
+++ main.java
@@ -1,5 +1,13 @@
-public class main
+import java.util.Random;
+
+public class Main
 {
+    Random r = new Random();
+    int zahl1;
+    int zahl2;
+    zahl1 = r.nextInt(2);
+    zahl2 = r.nextInt(2);
+    
     int[][] Feld =  {
                         { 1, 2, 3, 4, 5},
                         { 1, 2, 3, 4, 5},
