97e18b027d7bf8255c0954cfa521f9634073f60e
NamakiXVI <107111904+NamakiXVI@users.noreply.github.com>
2025-01-02 10:58:04 +0100

    Main Funktion erstellt

--- main.java
+++ main.java
@@ -7,7 +7,12 @@ public class Main
     int zahl2;
     zahl1 = r.nextInt(2);
     zahl2 = r.nextInt(2);
-    
+
+    public static void main(String[] args)
+    {
+        System.out.println(zahl1, zahl2);
+    }
+
     int[][] Feld =  {
                         { 1, 2, 3, 4, 5},
                         { 1, 2, 3, 4, 5},
