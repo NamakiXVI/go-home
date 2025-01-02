606b0ce8526c7d3a1d0fec779ef9d4d75fd0762c
NamakiXVI <107111904+NamakiXVI@users.noreply.github.com>
2025-01-02 11:14:28 +0100

    upload feld

--- Main.java
+++ Main.java
@@ -8,9 +8,13 @@ public class Main
     zahl1 = r.nextInt(2);
     zahl2 = r.nextInt(2);
 
+    int aktuelleFigur1;
+
     public static void main(String[] args)
     {
         System.out.println(zahl1, zahl2);
+        Figur f1 = new Figur("Blau", 0, 0);
+        Figur f1 = new Figur("Rot", 4, 4);
     }
 
     int[][] Feld =  {
