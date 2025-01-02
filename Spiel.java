a01388f9c04f1e02a488cb4739a604ec7628e9e2
NamakiXVI <107111904+NamakiXVI@users.noreply.github.com>
2025-01-02 11:25:24 +0100

    wassap

--- Spiel.java
+++ Spiel.java
@@ -1,4 +1,23 @@
 public class Spiel
 {
-    int[][] Feld = 
+    int[][] Feld = {
+        { 0, 1, 2, 3, 4},
+        { 0, 1, 2, 3, 4},
+        { 0, 1, 2, 3, 4},
+        { 0, 1, 2, 3, 4},
+        { 0, 1, 2, 3, 4},
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(Feld.length);
+    }
+
+    public void printFeld()
+    {
+        for(int i = 0; i < Feld.length; i++)
+        {
+            System.out.println(".");
+        }
+    }
 }
