39731a560a154ac9b19c5f855d8d306b10e80820
NamakiXVI <107111904+NamakiXVI@users.noreply.github.com>
2025-01-11 02:50:28 +0100

    Es wurde ein Unentschieden hinzugefügt, ein Logo in den statuspanel hinzugefügt und den Fehler behoben, wenn ein Spieler gewinnt, dass seine vorherige position aktualisiert wird

--- Spiel.java
+++ Spiel.java
@@ -8,6 +8,7 @@ public class Spiel extends JFrame
     private final int groesse = 5;
     private JButton[][] spielfeld;
     private JLabel statusLabel;
+    private JLabel logoLabel;
     
     Icon Haus = new ImageIcon(this.getClass().getResource("img/Haus.png"));
     Icon FRot = new ImageIcon("img/FigurRed.png");
@@ -18,6 +19,8 @@ public class Spiel extends JFrame
     Icon RCoin = new ImageIcon("img/RedCoin.png");
     Icon blue_on_house = new ImageIcon("img/B_on_house.png");
     Icon red_on_house = new ImageIcon("img/R_on_house.png");
+    Icon br_on_house = new ImageIcon("img/BR_on_house.png");
+    Icon go_home = new ImageIcon("img/go_home.png");
 
     
     JButton Coin1 = new JButton(RCoin);
@@ -31,55 +34,64 @@ public class Spiel extends JFrame
     Figur f1 = new Figur("Blau", 0, 0);
     Figur f2 = new Figur("Rot", 4, 4);
 
+    Random r = new Random();
+    int randomInt = r.nextInt(2);
     Figur aktuelleFigur1;
     Figur aktuelleFigur2;
-    Figur aktuellerSpieler;
+    Figur aktuellerSpieler = randomInt == 0 ? f1 : f2;
     
     public Spiel()
     {
         setTitle("Go Home Spiel");
-        setSize(670, 750);
+        setSize(670, 865);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
         setResizable(false);
+        setBackground(Color.LIGHT_GRAY);
 
         //  Erstellt das Spielfeld
         JPanel boardPanel = new JPanel(new GridLayout(groesse, groesse));
         spielfeld = new JButton[groesse][groesse];
         
-        // Spielfeld starten
+        // Spielfeld erstellen
         for (int y = 0; y < groesse; y++) 
         {
             for (int x = 0; x < groesse; x++) 
             {
                 JButton button = new JButton();
-                button.setFocusPainted(true);
                 spielfeld[y][x] = button;
                 boardPanel.add(button);
             }
         }
 
         // Erstellt den Status Panel
-        JPanel statusPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        JPanel statusPanel = new JPanel(new FlowLayout(0, 40, 0));
+        logoLabel = new JLabel(go_home);
+        statusPanel.add(logoLabel,BorderLayout.WEST);
+
         statusLabel = new JLabel("Spiel gestartet. Blau ist am Zug.");
         statusLabel.setFont(new Font("Pixelify Sans", Font.BOLD, 20));
-        statusPanel.add(statusLabel, BorderLayout.CENTER);
+        statusPanel.add(statusLabel, BorderLayout.EAST);
+
         statusPanel.add(Coin1,BorderLayout.EAST);
         Coin1.setBorderPainted(false);
         Coin1.setBackground(Color.WHITE);
+
         statusPanel.add(Coin2,BorderLayout.EAST);
         Coin2.setBorderPainted(false);
         Coin2.setBackground(Color.WHITE);
 
         // Fügt das Movement als buttons hinzu
         JPanel controlPanel = new JPanel(new GridLayout(1, 4));
-        String[] directions = {"Hoch", "Links", "Runter", "Rechts"};
+        String[] directions = {"HOCH", "LINKS", "RUNTER", "RECHTS"};
 
         for (int i = 0; i < directions.length; i++) 
         {
             JButton moveButton = new JButton(directions[i]);
-            String command = directions[i];
-            moveButton.addActionListener(e -> Bewegung(aktuelleFigur1, aktuelleFigur2, command));
+            moveButton.setFont(new Font("Kino MT", Font.BOLD, 15));
+            moveButton.setBackground(Color.LIGHT_GRAY);
+            String eingabe = directions[i];
+            moveButton.addActionListener(e -> Bewegung(aktuelleFigur1, aktuelleFigur2, eingabe));
             controlPanel.add(moveButton);
         }
 
@@ -129,26 +141,23 @@ public class Spiel extends JFrame
         {
             aktuelleFigur1 = f2;
             aktuelleFigur2 = f2;
-            aktuellerSpieler = f2;
+//            aktuellerSpieler = f2;
         }else if(zahl1 == 0 && zahl2 == 1)
         {
             aktuelleFigur1 = f1;
             aktuelleFigur2 = f2;
-            aktuellerSpieler = f1;
+//            aktuellerSpieler = f1;
         }else if(zahl1 == 1 && zahl2 == 0)
         {
             aktuelleFigur1 = f2;
             aktuelleFigur2 = f1;
-            aktuellerSpieler = f2;
+//            aktuellerSpieler = f2;
         }else if (zahl1 == 0 && zahl2 == 0)
         {
             aktuelleFigur1 = f1;
             aktuelleFigur2 = f1;
-            aktuellerSpieler = f1;
+//            aktuellerSpieler = f1;
         }
-
-        statusLabel.setText(aktuellerSpieler.farbe + " ist am Zug");
-        System.out.println(aktuellerSpieler.farbe + " ist am Zug");
     }
 
     public static void main(String[] args)
@@ -159,71 +168,71 @@ public class Spiel extends JFrame
 
     private boolean spielIstFertig() 
     {
+        if ((f2.x == 2 && f2.y == 2) && (f1.x == 2 && f1.y == 2)) 
+        {
+            JOptionPane.showMessageDialog(this, "Es gibt ein Unentschieden");
+            return true;
+        }
         if (f1.x == 2 && f1.y == 2) 
         {
-//            spielfeld[2][2].setIcon(blue_on_house);
-//            JOptionPane.showMessageDialog(this, f1.farbe + " hat gewonnen!");
+            JOptionPane.showMessageDialog(this, f1.farbe + " hat gewonnen!");
             return true;
         }
         if (f2.x == 2 && f2.y == 2) 
         {
-//            spielfeld[2][2].setIcon(red_on_house);
-//            JOptionPane.showMessageDialog(this, f2.farbe + " hat gewonnen!");
+            JOptionPane.showMessageDialog(this, f2.farbe + " hat gewonnen!");
             return true;
         }
         return false;
-    }   
+    }
 
     public void updateSpielfeld() 
     {
-
-        for(int y = 0; y <= 4; y++) 
+        for (int y = 0; y < groesse; y++) 
         {
-
-            for(int x = 0; x <= 4; x++) 
+            for (int x = 0; x < groesse; x++) 
             {
-
-                if (x == 2 && y == 2 && !spielIstFertig())
-                    spielfeld[2][2].setIcon(Haus);
-                else if (f1.x == x && f1.y == y && f2.x == x && f2.y == y)
-                    spielfeld[y][x].setIcon(FigurBR);
-                else if(f1.x == x && f1.y == y && !(f1.x == 2 && f1.y == 2))
+                // Prüfe ob jemand gewonnen hat
+                if (x == 2 && y == 2) 
                 {
-                    spielfeld[f1.y][f1.x].setIcon(FBlau);
-                    System.out.println("BLau: " + f1.x + f1.y);
+                    if (f1.x == 2 && f1.y == 2 && f2.x == 2 && f2.y == 2) 
+                    {
+                        spielfeld[2][2].setIcon(br_on_house); // Beide Figuren auf Haus
+                    } else if (f1.x == 2 && f1.y == 2) 
+                    {
+                        spielfeld[2][2].setIcon(blue_on_house); // Blaue Figur auf Haus
+                    } else if (f2.x == 2 && f2.y == 2) 
+                    {
+                        spielfeld[2][2].setIcon(red_on_house); // Rote Figur auf Haus
+                    } else 
+                    {
+                        spielfeld[2][2].setIcon(Haus); // Nur Haus
+                    }
                 }
-                else if(f2.x == x && f2.y == y && !(f2.x == 2 && f2.y == 2))
+                // Prüfe auf beide Figuren auf demselben Feld
+                else if (f1.x == x && f1.y == y && f2.x == x && f2.y == y) 
                 {
-                    spielfeld[f2.y][f2.x].setIcon(FRot);
-                    System.out.println("Rot: " + f2.x + f2.y);
+                    spielfeld[y][x].setIcon(FigurBR); // Beide Figuren
                 }
-                else if(f2.x == 2 && f2.y == 2)
+                // Prüfe auf die blaue Figur
+                else if (f1.x == x && f1.y == y) 
                 {
-                    spielfeld[2][2].setIcon(red_on_house);
-                    JOptionPane.showMessageDialog(this, f2.farbe + " hat gewonnen!");
-                    return;
+                    spielfeld[y][x].setIcon(FBlau); // Blaue Figur
                 }
-                else if(f1.x == 2 && f1.y == 2)
+                // Prüfe auf die rote Figur
+                else if (f2.x == x && f2.y == y) 
                 {
-                    spielfeld[2][2].setIcon(blue_on_house);
-                    JOptionPane.showMessageDialog(this, f1.farbe + " hat gewonnen!");
-                    return;
+                    spielfeld[y][x].setIcon(FRot); // Rote Figur
                 }
-                else
+                // sonst: Straßen-Icon
+                else {
                     spielfeld[y][x].setIcon(Street);
+                }
             }
         }
-
-        if (spielIstFertig()) 
-        {
-            statusLabel.setText(aktuellerSpieler.farbe + " hat das Spiel gewonnen!");
-            return;
-        }
-
         if (spielIstFertig()) return;
 
-        System.out.println(f1.x + " " + f1.y);
-        System.out.println(f2.x + " " + f2.y);
+        statusLabel.setText(aktuellerSpieler == f1 ? "Blau ist am Zug." : "Rot ist am Zug.");
 
         coinThrow();
         druckeAktuellenSpieler();
@@ -233,28 +242,37 @@ public class Spiel extends JFrame
     {
         if (spielIstFertig()) return;
 
+        Figur figur = aktuellerSpieler == null ? f1 : aktuellerSpieler;
+
         if (f_1 == f_2) 
         {
             switch (Richtung) 
             {
-                case "Hoch": aktuelleFigur1.bewegeHoch(); aktuelleFigur1.bewegeHoch(); break;
-                case "Links": aktuelleFigur1.bewegeLinks(); aktuelleFigur1.bewegeLinks(); break;
-                case "Runter": aktuelleFigur1.bewegeRunter(); aktuelleFigur1.bewegeRunter(); break;
-                case "Rechts": aktuelleFigur1.bewegeRechts(); aktuelleFigur1.bewegeRechts(); break;
+                case "HOCH": aktuelleFigur1.bewegeHoch(); aktuelleFigur1.bewegeHoch(); break;
+                case "LINKS": aktuelleFigur1.bewegeLinks(); aktuelleFigur1.bewegeLinks(); break;
+                case "RUNTER": aktuelleFigur1.bewegeRunter(); aktuelleFigur1.bewegeRunter(); break;
+                case "RECHTS": aktuelleFigur1.bewegeRechts(); aktuelleFigur1.bewegeRechts(); break;
             }
         } else 
         {
             switch (Richtung) 
             {
-                case "Hoch": aktuelleFigur1.bewegeHoch(); aktuelleFigur2.bewegeHoch(); break;
-                case "Links": aktuelleFigur1.bewegeLinks(); aktuelleFigur2.bewegeLinks(); break;
-                case "Runter": aktuelleFigur1.bewegeRunter(); aktuelleFigur2.bewegeRunter(); break;
-                case "Rechts": aktuelleFigur1.bewegeRechts(); aktuelleFigur2.bewegeRechts(); break;
+                case "HOCH": aktuelleFigur1.bewegeHoch(); aktuelleFigur2.bewegeHoch(); break;
+                case "LINKS": aktuelleFigur1.bewegeLinks(); aktuelleFigur2.bewegeLinks(); break;
+                case "RUNTER": aktuelleFigur1.bewegeRunter(); aktuelleFigur2.bewegeRunter(); break;
+                case "RECHTS": aktuelleFigur1.bewegeRechts(); aktuelleFigur2.bewegeRechts(); break;
             }
         }
 
         aktuelleFigur1.checkFeld();
         aktuelleFigur2.checkFeld();
+
+        System.out.println("Blaue Figur Coords: " + f1.x + " " + f1.y);
+        System.out.println("Rote Figur Coords: " + f2.x + " " + f2.y);
+        System.out.println("Aktuelle Figur 1 Coords: " + aktuelleFigur1.x + " " + aktuelleFigur1.y);
+        System.out.println("Aktuelle Figur 2 Coords: " + aktuelleFigur2.x + " " + aktuelleFigur2.y);
+
+        aktuellerSpieler = (figur == f1) ? f2 : f1;
 
         updateSpielfeld();
     }
